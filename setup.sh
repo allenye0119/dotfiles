301fdbfba6e268e944461ffc428be8cb6c88779a
Allen Ye <allenye0119@gmail.com>
2016-12-30 16:21:00 +0800

    Modify setup.ssh

--- setup.sh
+++ setup.sh
@@ -5,15 +5,12 @@ ln -sfv ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 # tmux
 ln -sfv ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -sfv ~/.dotfiles/tmux/tmux ~/.tmux
-ln -sfv ~/.dotfiles/tmuxinator ~/.tmuxinator
 
 # git
 ln -sfv ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -sfv ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 
 # vim
-ln -sfv ~/.dotfiles/vim/vim ~/.vim
 ln -sfv ~/.dotfiles/vim/vimrc ~/.vimrc
 
 # R
