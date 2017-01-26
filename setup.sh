698e29ee0bf13c021e5244a61012df30dc69f006
Allen Ye <allenye0119@gmail.com>
2017-01-27 00:31:00 +0800

    A bunch of update

--- setup.sh
+++ setup.sh
@@ -1,36 +1,58 @@
 #!/bin/sh
 
+check_dir () {
+	if [ ! -d $1 ]; then
+		mkdir $1
+	fi
+}
+
+link_file () {
+	ln -sfv $1 $2
+}
+
+link_files () {
+	for f in `ls $1`; do
+		ln -sfv $1/$f $2/$f
+	done
+}
+
 # zsh
-ln -sfv ~/.dotfiles/zsh/zshrc ~/.zshrc
+link_file ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 # tmux
-ln -sfv ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+link_file ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-# git
-ln -sfv ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -sfv ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+# tmuxinator
+check_dir ~/.bin
+link_file ~/.dotfiles/tmuxinator/tmuxinator.zsh ~/.bin/tmuxinator.zsh
+
+check_dir ~/.tmuxinator
+link_files ~/.dotfiles/tmuxinator/templates ~/.tmuxinator
 
 # vim
-ln -sfv ~/.dotfiles/vim/vimrc ~/.vimrc
+check_dir ~/.vim
+link_file ~/.dotfiles/vim/vimrc ~/.vimrc
 
-if [ ! -d ~/.vim/UltiSnips ]; then
-	mkdir ~/.vim/UltiSnips
-fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
-for f in `ls ~/.dotfiles/vim/UltiSnips`; do
-	ln -sfv ~/.dotfiles/vim/UltiSnips/$f ~/.vim/UltiSnips/$f
-done
+check_dir ~/.vim/UltiSnips
+link_files ~/.dotfiles/vim/UltiSnips ~/.vim/UltiSnips
+
+check_dir ~/.vim/colors
+link_file ~/.dotfiles/vim/colors/solarized.vim ~/.vim/colors/solarized.vim
+
+# git
+link_file ~/.dotfiles/git/gitconfig ~/.gitconfig
+link_file ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 
 # R
-ln -sfv ~/.dotfiles/R/Rprofile ~/.Rprofile
+link_file ~/.dotfiles/R/Rprofile ~/.Rprofile
 
 # ptpython
-if [ ! -d ~/.ptpython ]; then
-	mkdir ~/.ptpython
-fi
-
-ln -sfv ~/.dotfiles/ptpython/config.py ~/.ptpython/config.py
+check_dir ~/.ptpython
+link_file ~/.dotfiles/ptpython/config.py ~/.ptpython/config.py
 
 # dircolors
-ln -sfv ~/.dotfiles/dir_colors/dircolors.256dark ~/.dir_colors
+link_file ~/.dotfiles/dir_colors/dircolors.256dark ~/.dir_colors
 
