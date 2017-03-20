8a9d2521d5130bfa18dc9eb9a0faa2ab70b4f802
Allen Ye <allenye0119@gmail.com>
2017-03-20 16:56:04 +0800

    zsh: Add ipython alias

--- zsh/custom/plugins/alias/alias.plugin.zsh
+++ zsh/custom/plugins/alias/alias.plugin.zsh
@@ -1,7 +1,8 @@
+alias zshconfig="vim ~/.zshrc"
+alias sourcezsh="source ~/.zshrc"
+
 if whence trash-put > /dev/null; then
 	alias tp="trash-put"
 fi
 
-alias zshconfig="vim ~/.zshrc"
-alias sourcezsh="source ~/.zshrc"
-
+lias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
