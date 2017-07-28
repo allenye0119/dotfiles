4d4e2f5c37346e9fa17990e1312089645ee32735
Allen Ye <allenye0119@gmail.com>
2017-07-28 20:51:16 +0800

    commit before destroying my system

--- zsh/custom/plugins/alias/alias.plugin.zsh
+++ zsh/custom/plugins/alias/alias.plugin.zsh
@@ -5,6 +5,4 @@ if whence trash-put > /dev/null; then
 	alias tp="trash-put"
 fi
 
-alias python="python3.6"
-alias pip="pip3.6"
-alias ipy="python3.6 -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
+alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
