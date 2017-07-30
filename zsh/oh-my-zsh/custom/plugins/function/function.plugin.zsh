45e87d9006a62dd4b4797846e93e9f7eb759914b
Allen Ye <allenye0119@gmail.com>
2017-07-31 00:12:19 +0800

    zsh: Add pip installed check to cpip function

--- zsh/oh-my-zsh/custom/plugins/function/function.plugin.zsh
+++ zsh/oh-my-zsh/custom/plugins/function/function.plugin.zsh
@@ -6,6 +6,12 @@ cpip() {
 
 	[ -z "$CONDA_DEFAULT_ENV" ] && echo "$ERROR_MSG" && return 1
 
+	ERROR_MSG='Pip not installed in current conda environment.'
+	ERROR_MSG="$ERROR_MSG\nUse \`conda install pip\`"
+	ERROR_MSG="$ERROR_MSG to install pip in current conda environment."
+
+	[ -e "$CONDA_PREFIX/bin/pip" ] || echo "$ERROR_MSG" && return 2
+
 	PIP="$CONDA_PREFIX/bin/pip"
 	"$PIP" "$@"
 }
